490c516c504ab826526bf78d4701c7ed73c4ffab
Jelle Spekken <jelle.spekken@centagon.com>
2016-09-26 14:52:51 +0200

    docs removed

--- src/js/lib/Bootstrapper.js
+++ src/js/lib/Bootstrapper.js
@@ -1,12 +1,4 @@
 /* eslint-disable no-console */
-/**
- * This file is part of the Centagon Primer package.
- *
- * (c) Centagon <contact@centagon.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 export default class {
 
