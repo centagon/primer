0c0677f107ea8eacf95e54dc50cdd9fad9dbe4ee
Jelle Spekken <jelle.spekken@centagon.com>
2016-10-04 11:02:44 +0200

    string utilities added

--- src/js/lib/Util.js
+++ src/js/lib/Util.js
@@ -99,3 +99,35 @@ export class Window {
         window.open(url, '', `width=${width},height=${height},top=${top},left=${left}`);
     }
 }
+
+export class String {
+
+    /**
+     * Decode a string into an object.
+     *
+     * @param  {string}  str
+     *
+     * @returns {*}
+     */
+    static toObject(str) {
+        return str.split('&').reduce((ret, param) => {
+            let parts = param.replace(/\+/g, ' ').split('=');
+            let key = parts[0];
+            let val = parts[1];
+
+            key = decodeURIComponent(key);
+
+            val = val === undefined ? null: decodeURIComponent(val);
+
+            if (!ret.hasOwnProperty(key)) {
+                ret[key] = val;
+            } else if (Array.isArray(ret[key])) {
+                ret[key].push(val);
+            } else {
+                ret[key] = [ret[key], val];
+            }
+
+            return ret;
+        }, {});
+    }
+}
