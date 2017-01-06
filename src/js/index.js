36b342bcaa8c21c6adeb10342140eb9364342afd
René Heijnemans <rene.heijnemans@centagon.com>
2017-01-06 13:13:54 +0100

    npm package

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+import Anchor from './lib/Anchor';
+import Bootstrapper from './lib/Bootstrapper';
+import Equalizer from './lib/Equalizer';
+import EventTriggers from './lib/EventTriggers';
+import Form from './lib/Form';
+import LazyLoader from './lib/LazyLoader';
+import MediaQuery from './lib/MediaQuery';
+import Scroll from './lib/Scroll';
+import Social from './lib/Social';
+import Util from './lib/Util';
+
+export {
+    Anchor,
+    Bootstrapper,
+    Equalizer,
+    EventTriggers,
+    Form,
+    LazyLoader,
+    MediaQuery,
+    Scroll,
+    Social,
+    Util,
+}
---
+++ src/js/lib/Social.js
@@ -0,0 +1,7 @@
+import Networks from './Social/Networks';
+import Share from './Social/Share';
+
+export {
+    Networks,
+    Share
+}
